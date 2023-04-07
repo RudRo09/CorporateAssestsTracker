03a91ad8e8e547efef49f59d5b0915abc6466051
Shafin Islam Rudro <shafin.rudro@northsouth.edu>
2023-04-07 10:34:27 -0700

    code #3: Defined viewsets for the models.
    Created CompanyViewSet, EmployeeViewSet, DeviceViewSet, and DeviceLogViewSet. Each viewset defines the queryset, serializer_class, and permission_classes for the respective model.

--- CorporateAssetsTracker/assets/views.py
+++ CorporateAssetsTracker/assets/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from .models import Company, Employee, Device, DeviceLog
+from .serializers import CompanySerializer, EmployeeSerializer, DeviceSerializer, DeviceLogSerializer
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class DeviceViewSet(viewsets.ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class DeviceLogViewSet(viewsets.ModelViewSet):
+    queryset = DeviceLog.objects.all()
+    serializer_class = DeviceLogSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
